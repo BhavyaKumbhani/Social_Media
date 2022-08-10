98038c4b235c36c231d2af5542617bfe1706e428
Bhavya <bhavya144.rejoice@gmail.com>
2022-08-10 16:59:40 +0530

    main sectiondd

---
+++ src/components/rightbar/Right.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Right.css'
+
+const Right = () => {
+    return (
+        <div className='RightBar'>
+            Rightbar
+        </div>
+    );
+}
+
+export default Right;
